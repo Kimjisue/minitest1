474547770ec3b9e6922ee0263dc4ca73bc7a26c3
Kimjisue <22100194@handong.ac.kr>
2022-04-25 19:08:17 +0900

    test8

--- manager.c
+++ manager.c
@@ -56,6 +56,18 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	 fp = fopen("product.txt","rt");
+        if(fp == NULL){
+       		printf("파일 없음");
+        	return 0;
+        }
+        for(count=0;count<100;count++){
+       		fscanf(fp,"%s",p[count].name);
+        	if(feof(fp)) break;
+       		fscanf(fp, "%s %d %d\n", p[count].name, p[count].weight, p[count].price);
+        	if(feof(fp)) break;
+        }
+        fclose(fp);
 
 
 
